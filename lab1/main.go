23674d53d83f72a781afc979a2f90371a3c51079
Uliana <167175946+Ulyanapupup@users.noreply.github.com>
2026-09-22 21:08:44 +0800

    Update main.go

--- lab1/main.go
+++ lab1/main.go
@@ -23,6 +23,26 @@ func main() {
 		return
 	}
 
+	if month < 1 || month > 12 || day < 1 || day > 31 {
+		fmt.Println("Некорректная дата")
+		return
+	}
+
+	if month == 2 && day > 29 {
+		fmt.Println("Некорректная дата: февраль не может иметь более 29 дней")
+		return
+	}
+
+	if (month == 4 || month == 6 || month == 9 || month == 11) && day > 30 {
+		fmt.Println("Некорректная дата: этот месяц не может иметь более 30 дней")
+		return
+	}
+
+	if year%400 != 0 && year%100 == 0 && month == 2 && day > 28 {
+		fmt.Println("Некорректная дата: в невисокосный год февраль не может иметь более 28 дней")
+		return
+	}
+
 	date := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.Local)
 
 	result := dayToNewYear(date)
